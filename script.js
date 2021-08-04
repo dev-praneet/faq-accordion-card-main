80c919120e2c21e3ba89e57dc82d39f797d5dab6
Praneet Singh <Praneet@Praneets-MacBook-Air.local>
2021-08-04 10:01:25 +0530

    done for the most part, yet to give a max-size to image

--- script.js
+++ script.js
@@ -23,25 +23,36 @@ for (let i = 0; i < questionBlocks.length; i++) {
     });
 }
 
+
+const root = document.querySelector(':root');
+const containerPaddingTop = getComputedStyle(root).getPropertyValue('--container_padding_top');
+const mobileWidth = getComputedStyle(root).getPropertyValue('--mobile_width');
+
 const imageBox = document.getElementById('image-box');
 imageBox.addEventListener("load", () => {
+    if (window.innerWidth <= mobileWidth.slice(0, -2)) {
     imageBox.style.marginLeft = -0.5 * imageBox.clientWidth + "px";
-    imageBox.style.top = -0.45 * imageBox.clientHeight - containerPaddingTop.slice(0, -2) + "px"; 
-})
+    imageBox.style.top = -0.45 * imageBox.clientHeight - containerPaddingTop.slice(0, -2) + "px";
+    } 
+});
 window.addEventListener("resize", () => {
+    if (window.innerWidth <= mobileWidth.slice(0,-2)) {
     imageBox.style.marginLeft = -0.5 * imageBox.clientWidth + "px"; 
-    imageBox.style.top = -0.45 * imageBox.clientHeight - containerPaddingTop.slice(0, -2) + "px"; 
+    imageBox.style.top = -0.45 * imageBox.clientHeight - containerPaddingTop.slice(0, -2) + "px";
+    } else {
+        console.log('else block was executed');
+        imageBox.style.marginLeft = null;
+        imageBox.style.top = null;
+    }
 });
 
 const imageWoman = document.getElementById('image-woman');
 imageBox.addEventListener("load", () => {
     imageWoman.style.marginLeft = -0.5 * imageWoman.clientWidth + "px";
     imageWoman.style.top = -0.6 * imageWoman.clientHeight - containerPaddingTop.slice(0, -2) + "px";
-})
+});
 window.addEventListener("resize", () => {
     imageWoman.style.marginLeft = -0.5 * imageWoman.clientWidth + "px"; 
     imageWoman.style.top = -0.6 * imageWoman.clientHeight - containerPaddingTop.slice(0, -2) + "px";
 });
 
-const root = document.querySelector(':root');
-const containerPaddingTop = getComputedStyle(root).getPropertyValue('--container_padding_top');
